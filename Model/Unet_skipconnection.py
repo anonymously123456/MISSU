162469363605a9cbf4c56f322dc3069cc6151218
Ashelley <56376393+wangn123@users.noreply.github.com>
2022-10-15 11:24:11 -0700

    Update Unet_skipconnection.py

--- Model/Unet_skipconnection.py
+++ Model/Unet_skipconnection.py
@@ -2,7 +2,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
-# adapt from https://github.com/MIC-DKFZ/BraTS2017
 
 
 def normalization(planes, norm='gn'):
@@ -55,6 +54,26 @@ class EnBlock(nn.Module):
 
         return y
 
+    
+class DACblock(nn.Module):
+    def __init__(self, channel):
+        super(DACblock, self).__init__()
+        self.dilate1 = nn.Conv2d(channel, channel, kernel_size=3, dilation=1, padding=1)
+        self.dilate2 = nn.Conv2d(channel, channel, kernel_size=3, dilation=3, padding=3)
+        self.dilate3 = nn.Conv2d(channel, channel, kernel_size=3, dilation=5, padding=5)
+        self.conv1x1 = nn.Conv2d(channel, channel, kernel_size=1, dilation=1, padding=0)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+                if m.bias is not None:
+                    m.bias.data.zero_()
+
+    def forward(self, x):
+        dilate1_out = nonlinearity(self.dilate1(x))
+        dilate2_out = nonlinearity(self.conv1x1(self.dilate2(x)))
+        dilate3_out = nonlinearity(self.conv1x1(self.dilate2(self.dilate1(x))))
+        dilate4_out = nonlinearity(self.conv1x1(self.dilate3(self.dilate2(self.dilate1(x)))))
+        out = x + dilate1_out + dilate2_out + dilate3_out + dilate4_out
+        return out
 
 class EnDown(nn.Module):
     def __init__(self, in_channels, out_channels):
