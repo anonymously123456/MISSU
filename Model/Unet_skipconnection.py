78e179b3ce9ac3ae89a49609837fa021321510cc
Ashelley <56376393+wangn123@users.noreply.github.com>
2022-10-15 11:38:14 -0700

    Update Unet_skipconnection.py

--- Model/Unet_skipconnection.py
+++ Model/Unet_skipconnection.py
@@ -74,6 +74,36 @@ class DACblock(nn.Module):
         dilate4_out = nonlinearity(self.conv1x1(self.dilate3(self.dilate2(self.dilate1(x)))))
         out = x + dilate1_out + dilate2_out + dilate3_out + dilate4_out
         return out
+    
+ class SPPblock(nn.Module):
+    def __init__(self, in_channels):
+        super(SPPblock, self).__init__()
+        self.pool1 = nn.MaxPool2d(kernel_size=[2, 2], stride=2)
+        self.pool2 = nn.MaxPool2d(kernel_size=[3, 3], stride=3)
+        self.pool3 = nn.MaxPool2d(kernel_size=[5, 5], stride=5)
+        self.pool4 = nn.MaxPool2d(kernel_size=[6, 6], stride=6)
+
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=1, kernel_size=1, padding=0)
+
+    def forward(self, x):
+        self.in_channels, h, w = x.size(1), x.size(2), x.size(3)
+        self.layer1 = F.upsample(self.conv(self.pool1(x)), size=(h, w), mode='bilinear')
+        self.layer2 = F.upsample(self.conv(self.pool2(x)), size=(h, w), mode='bilinear')
+        self.layer3 = F.upsample(self.conv(self.pool3(x)), size=(h, w), mode='bilinear')
+        self.layer4 = F.upsample(self.conv(self.pool4(x)), size=(h, w), mode='bilinear')
+        print('x.size(1)',x.size(1))
+        print('x.size(2)',x.size(2))
+        print('x.size(3)',x.size(3))
+        print('self.layer1',self.layer1.shape)
+        print('self.layer2',self.layer2.shape)
+        print('self.layer3',self.layer3.shape)
+        print('self.layer4',self.layer4.shape)
+
+        out = torch.cat([self.layer1, self.layer2, self.layer3, self.layer4, x], 1)
+        # print('out',out.shape)
+
+        return out
+
 
 class EnDown(nn.Module):
     def __init__(self, in_channels, out_channels):
@@ -94,38 +124,55 @@ class Unet(nn.Module):
         self.InitConv = InitConv(in_channels=in_channels, out_channels=base_channels, dropout=0.2)
         self.EnBlock1 = EnBlock(in_channels=base_channels)
         self.EnDown1 = EnDown(in_channels=base_channels, out_channels=base_channels*2)
+        self.dblock = DACblock(out_channels=base_channels*2)
+        self.spp = SPPblock(out_channels=base_channels*2)
+
 
         self.EnBlock2_1 = EnBlock(in_channels=base_channels*2)
         self.EnBlock2_2 = EnBlock(in_channels=base_channels*2)
         self.EnDown2 = EnDown(in_channels=base_channels*2, out_channels=base_channels*4)
+        self.dblock = DACblock(out_channels=base_channels*4)
+        self.spp = SPPblock(out_channels=base_channels*4)
 
         self.EnBlock3_1 = EnBlock(in_channels=base_channels * 4)
         self.EnBlock3_2 = EnBlock(in_channels=base_channels * 4)
         self.EnDown3 = EnDown(in_channels=base_channels*4, out_channels=base_channels*8)
+        self.dblock = DACblock(out_channels=base_channels*8)
+        self.spp = SPPblock(out_channels=base_channels*8)
 
         self.EnBlock4_1 = EnBlock(in_channels=base_channels * 8)
         self.EnBlock4_2 = EnBlock(in_channels=base_channels * 8)
         self.EnBlock4_3 = EnBlock(in_channels=base_channels * 8)
         self.EnBlock4_4 = EnBlock(in_channels=base_channels * 8)
+        self.dblock = DACblock(out_channels=base_channels*8)
+        self.spp = SPPblock(out_channels=base_channels*8)
 
     def forward(self, x):
         x = self.InitConv(x)       # (1, 16, 128, 128, 128)
 
         x1_1 = self.EnBlock1(x)
         x1_2 = self.EnDown1(x1_1)  # (1, 32, 64, 64, 64)
+        x1_2 = self.dblock(x1_1)
+        x1_2 = self.spp(x1_1)
 
         x2_1 = self.EnBlock2_1(x1_2)
         x2_1 = self.EnBlock2_2(x2_1)
         x2_2 = self.EnDown2(x2_1)  # (1, 64, 32, 32, 32)
+        x2_2 = self.dblock(x2_1)
+        x2_2 = self.spp(x2_1)
 
         x3_1 = self.EnBlock3_1(x2_2)
         x3_1 = self.EnBlock3_2(x3_1)
         x3_2 = self.EnDown3(x3_1)  # (1, 128, 16, 16, 16)
+        x2_2 = self.dblock(x3_2)
+        x2_2 = self.spp(x3_2)
 
         x4_1 = self.EnBlock4_1(x3_2)
         x4_2 = self.EnBlock4_2(x4_1)
         x4_3 = self.EnBlock4_3(x4_2)
         output = self.EnBlock4_4(x4_3)  # (1, 128, 16, 16, 16)
+        x2_2 = self.dblock(x4_3)
+        x2_2 = self.spp(x4_3)
 
         return x1_1,x2_1,x3_1,output
 
